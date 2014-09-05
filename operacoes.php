5744c5c8473c2a9009604bc2a954bb79bc485acc
31442897 <31442897@087998-P33S105.macklabs.br>
2014-09-05 08:40:45 -0300

    operacoes

---
+++ operacoes.php
@@ -0,0 +1,23 @@
+
+
+<?php
+            function soma($x, $y){
+                $z = $x + $y;
+                return $z;
+            }
+            
+            function subtracao($x, $y){
+                $z = $x - $y;
+                return $z;
+            }
+            
+            function multiplicacao($x, $y){
+                $z = $x * $y;
+                return $z;
+            }
+            
+            function divisao ($x, $y){
+                $z = $x / $y;
+                return $z;
+            }
+?>
